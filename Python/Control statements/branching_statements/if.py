447f83768e59e671f3582c3f30b7f763c678e332
Tharun <rt7684002@gmail.com>
2024-08-23 00:10:42 +0530

    branching

---
+++ Python/Control statements/branching_statements/if.py
@@ -0,0 +1,36 @@
+'''a = int(input("a:"))
+b = int(input("b:"))
+c = a>>b 
+print(c)
+d = a<<b
+print(d)
+if  c>d:
+    print(" right shift is greater")'''
+# else:
+#     print(" left shift is greater")      
+
+
+'''a = int(input("a:"))
+b = int(input("b:"))        
+c = int(input("c:"))
+z = a+b+c
+print(z)
+if (z/3)%2==0:
+    print("even")'''
+# else:
+#     print("odd")   
+
+
+'''a = int(input("a:"))
+if a<=10:
+    print("hello")'''
+
+'''a = int(input("a:"))
+if a>-999 and a<-99 or a>99 and a<999:
+    print("true")'''
+
+'''a = int(input("a:"))
+if a is 99:
+    print("yoyo")'''
+
+
---
+++ Python/Control statements/branching_statements/if_else.py
@@ -0,0 +1,55 @@
+'''a = int(input("a:"))
+b = int(input("b:"))
+c = a+b
+print(c)
+if c%2==0:
+    print("even number")
+else:
+    print("odd number")    '''
+
+
+'''a=int(input("a:"))
+b=int(input("b:"))
+c=int(input("c:"))
+d=int(input("d:"))
+e=int(input("e:"))
+z=a+b+c+d+e
+
+print((z/500)*100)
+if ((z/500)*100)>=35:
+    print("pass")
+else:
+    print("fail")   ''' 
+
+
+'''a=int(input("a:"))
+b=int(input("b:"))
+
+if a is b:
+    print("same value")
+else:
+    print("different values")'''
+        
+
+
+'''a=input("a:")
+b=input("b:")
+c=input("c:")
+print(c)   
+if c in (a and  b):
+    print("true")
+else:
+    print("false")  '''       
+
+
+
+'''a = int(input("a: "))
+b = int(input("b: "))
+c = a + b
+print(c)
+if (c//2)*2==0:
+    print("even number")
+else:
+    print("odd number") '''   
+
+
