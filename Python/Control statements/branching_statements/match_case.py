bb003c1c04c4e6b46987224876667f174a9ede49
Tharun <rt7684002@gmail.com>
2024-08-28 15:31:27 +0530

    match case

---
+++ Python/Control statements/branching_statements/match_case.py
@@ -0,0 +1,39 @@
+           #calculator
+'''a = int(input("a:"))
+b = int(input("b:"))
+opp =input("enter the operation:")
+match opp:
+    case '+':
+        print(a+b) 
+    case '-':
+        print(a-b)
+    case '*':
+        print(a*b)
+    case '/':
+        print(a/b)      
+    case _:
+        print("not a operation")'''
+
+    
+                  # (or)
+             #using while loop
+'''while True:
+    a = int(input("a:"))
+    b = int(input("b:"))
+    opp =input("enter the operation:")
+    match opp:
+        case '+':
+           print(a+b) 
+        case '-':
+           print(a-b)
+        case '*':
+           print(a*b)
+        case '/':
+           print(a/b) 
+        case 'cancle':
+           break         
+        case _:
+           print("not a operation") '''
+
+
+                  
--- Python/Control statements/branching_statements/nested_if.py
+++ Python/Control statements/branching_statements/nested_if.py
@@ -31,3 +31,69 @@ if a>=740 and a<=900:
         print("u r not eligible of loan")     
 else:
     print("u r not eligible")  '''      
+
+
+    #election eligibility
+'''gen = input("enter the gender \nmale\tfemale\tother\noption:").lower()
+if gen in ['male','female','other']:
+    age = float(input("enter the age:"))
+    if gen in ['male','other']:
+        if age>=18 and age<=65:
+            print("u r eligible to vote")
+        else:
+            print("not eligible to vote")
+    else:
+        if age>=21 and age<=45: 
+            print("u r eligible to vote")
+        else:
+            print("not eligible to vote")    
+else:
+    print("invalid gender")      ''' 
+
+                    #(or)
+
+'''gen = input("enter the gender\nmale\tfemale\tother\noption:")
+if gen in ['male','female','other']:
+   age = float(input("enter the age:"))
+   name = input("name:")
+   if((gen in ['male','other']) and (age in range(18,66))) or (gen == 'female' and (age in range(21,46))):
+           print(f"{name} is eligible to vote")
+   else:
+           print(f"{name} is not eligible to vote")
+else:
+   print(f"{gen} is an invalid gender")   '''                                                                   
+
+
+         #sample calculator
+'''operator = input("enter the operation\n +\t-\t*\t/\noperation:")
+if operator in ['+','-','*','/']:
+    a = float(input("enter the a:"))
+    b = float(input("enter the b:"))
+    if operator =='+':
+        print(a+b)
+    elif operator =='-':
+        print(a-b)
+    elif operator =='*':
+        print(a*b)
+    else:
+        print(a/b)            
+
+else:
+    print("invalid operator") '''
+
+
+
+#Write a program that checks if a number is within the range of 10 to 50 (inclusive) and if it's divisible by 3, print "The number is within the range and divisible by 3".
+'''a = int(input(" enter the a : "))
+if  a>=10 and a<=50 :
+    if a%3==0 :
+        print("number is within the range and divisible by 3")
+    else:
+        print("number is  within the range and  not divisible by 3")
+
+else:
+    print("it is not in the range") '''
+
+
+
+
